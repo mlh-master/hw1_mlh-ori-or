7256f2c7f5295b5bc28daac85869f42a274e2743
LAPTOP-OR19\OriR <ori.ra@campus.technion.ac.il>
2020-11-20 15:37:30 +0200

    committed 20 Nov part 1 only clean_data python file was changed

--- clean_data.py
+++ clean_data.py
@@ -17,7 +17,8 @@ def rm_ext_and_nan(CTG_features, extra_feature):
     :return: A dictionary of clean CTG called c_ctg
     """
     # ------------------ IMPLEMENT YOUR CODE HERE:------------------------------
-
+    c_ctg = CTG_features.apply(lambda col: pd.to_numeric(col, errors='coerce')).dropna().drop(
+        "{}".format(extra_feature), 1)
     # --------------------------------------------------------------------------
     return c_ctg
 
@@ -31,6 +32,49 @@ def nan2num_samp(CTG_features, extra_feature):
     """
     c_cdf = {}
     # ------------------ IMPLEMENT YOUR CODE HERE:------------------------------
+    c_run = CTG_features.apply(lambda col: pd.to_numeric(col, errors='coerce')).drop("{}".format(extra_feature), 1)
+    # c_run = c_run.drop(c_run.index[0])
+
+    def removeNanWithSamples(col):
+        col_without_nan = col.dropna()
+        random_col = np.random.choice(col_without_nan, col.size)
+        col[col.isnull()] = random_col[col.isnull()]
+        return col
+    c_cdf = c_run.apply(removeNanWithSamples)
+
+    # temp = rm_ext_and_nan(CTG_features, extra_feature)
+    #
+    # for c in c_run.keys():
+    #     col = c_run[c]
+    #     col_without_nan = col.dropna()
+    #     random_col = np.random.choice(col_without_nan, col.size)
+    #     col[col.isnull()] = random_col[col.isnull()]
+    #     c_run[c] = col
+    #     temp = c_run
+    #     pd.Series(np.random.choice(temp[c].dropna()) )
+
+
+    #     cdf = np.cumsum(temp[c]) / np.sum(temp[c])
+    #     sort_temp = temp[c].sort_values(ascending=True)
+    #     num_of_nan = c_run[c].isnull().sum()
+    #     rand_vec = np.random.uniform(0, 1, num_of_nan)
+    #     sampeled_vec = []
+    #     j = 0
+    #
+    #     for i in range(num_of_nan):
+    #         for k in range(cdf.shape[0] - 1):
+    #             if ((rand_vec[i] <= cdf.values[k + 1]) & (rand_vec[i] >= cdf.values[k])):
+    #                 sampeled_vec.append(sort_temp.values[k])
+    #                 break
+    #
+    #     for row in range(c_run.shape[0]):
+    #         if (np.isnan(c_run[c].values[row])):
+    #             c_run[c].values[row] = sampeled_vec[j]
+    #             j = j + 1
+    #             if (j == num_of_nan):
+    #                 break
+    #
+    #     c_cdf.update({c: c_run[c]})
 
     # -------------------------------------------------------------------------
     return pd.DataFrame(c_cdf)
@@ -43,7 +87,15 @@ def sum_stat(c_feat):
     :return: Summary statistics as a dicionary of dictionaries (called d_summary) as explained in the notebook
     """
     # ------------------ IMPLEMENT YOUR CODE HERE:------------------------------
-
+    d_summary = {}
+    for c in c_feat.keys():
+        minimum = c_feat[c].min()
+        quar = c_feat[c].quantile(0.25)
+        med = c_feat[c].median()
+        tri_quart = c_feat[c].quantile(0.75)
+        maximum = c_feat[c].max()
+        new = {"min": minimum, "Q1": quar, "median": med, "Q3": tri_quart, "max": maximum}
+        d_summary.update({c: new})
     # -------------------------------------------------------------------------
     return d_summary
 
@@ -57,7 +109,13 @@ def rm_outlier(c_feat, d_summary):
     """
     c_no_outlier = {}
     # ------------------ IMPLEMENT YOUR CODE HERE:------------------------------
-
+    for c in c_feat.keys():
+        iqr = d_summary[c]['Q3'] - d_summary[c]['Q1']
+        upper = d_summary[c]['Q3'] + iqr * 1.5
+        lower = d_summary[c]['Q1'] - iqr * 1.5
+        c_feat_coll = c_feat[c]
+        new = c_feat_coll[(c_feat_coll <= upper) & (c_feat_coll >= lower)]
+        c_no_outlier.update({c: new})
     # -------------------------------------------------------------------------
     return pd.DataFrame(c_no_outlier)
 
@@ -71,7 +129,8 @@ def phys_prior(c_cdf, feature, thresh):
     :return: An array of the "filtered" feature called filt_feature
     """
     # ------------------ IMPLEMENT YOUR CODE HERE:-----------------------------
-
+    filt_feature = c_cdf[feature]
+    filt_feature = np.array(filt_feature[filt_feature <= thresh])
     # -------------------------------------------------------------------------
     return filt_feature
 
@@ -87,6 +146,27 @@ def norm_standard(CTG_features, selected_feat=('LB', 'ASTV'), mode='none', flag=
     """
     x, y = selected_feat
     # ------------------ IMPLEMENT YOUR CODE HERE:------------------------------
+    nsd_res = CTG_features.copy()
+    if (mode == 'standard'):
+        for i in nsd_res.keys():
+            nsd_res[i] = (nsd_res[i] - nsd_res[i].median()) / nsd_res[i].std()
 
-    # -------------------------------------------------------------------------
+    if (mode == 'MinMax'):
+        for i in nsd_res.keys():
+            nsd_res[i] = (nsd_res[i] - nsd_res[i].min()) / (nsd_res[i].max() - nsd_res[i].min())
+
+    if (mode == 'mean'):
+        for i in nsd_res.keys():
+            nsd_res[i] = (nsd_res[i] - nsd_res[i].mean()) / (nsd_res[i].max() - nsd_res[i].min())
+
+    if (flag == True):
+        import matplotlib.pyplot as plt
+        plt.hist(nsd_res[x], bins=80, label=x)
+        plt.hist(nsd_res[y], bins=80, label=y)
+        plt.ylabel('Frequency')
+        plt.xlabel('Values')
+        plt.legend(loc='upper right')
+        plt.show()
+
+        # -------------------------------------------------------------------------
     return pd.DataFrame(nsd_res)
